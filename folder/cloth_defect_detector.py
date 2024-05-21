c4c51ebc83317b6031278bfab56afba2b434ac7c
neeraj10122004 <119481961+neeraj10122004@users.noreply.github.com>
2024-05-21 13:42:59 +0530

    Add files via upload

---
+++ folder/cloth_defect_detector.py
@@ -0,0 +1,18 @@
+import google.generativeai as genai
+import PIL.Image
+
+print(genai.__version__)
+
+gemini_api_key = "AIzaSyDYfQmz7AHGbtvY4l5UJGVCa8JJgJrDjaQ"
+
+genai.configure(api_key = gemini_api_key)
+def get_response(prompt,command):
+    model = genai.GenerativeModel('gemini-pro-vision')
+    response = model.generate_content([command,prompt])
+    return response
+
+for i in range(1,7):
+    img = PIL.Image.open(f'{i}.jpeg')
+    responce=get_response(img,"check for defects and send message defects if any defect is present else no defects")
+    print(responce.text)
+
